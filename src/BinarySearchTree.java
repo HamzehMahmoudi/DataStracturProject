bdbc3afd5e1fbf35d9b8f945e83a2b578d3cdd7b
crooked2 <46393208+crooked2@users.noreply.github.com>
2020-06-29 04:10:58 +0430

    insert method added

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
 public class BinarySearchTree {
+    class Node{
+        String FirstName , LastName ;
+        int ID;
+        Node left,right;
+
+        public Node(int ID , String firstName, String lastName ) {
+            FirstName = firstName;
+            LastName = lastName;
+            this.ID = ID;
+            this.right=null;
+            this.right=null;
+        }
+    }
+    Node root;
+
+
+    public BinarySearchTree() {
+        this.root = null;
+    }
+    public void Insert(int id , String fname , String lname){
+        Node node=new Node(id,fname,lname);
+        if(root==null){
+            root=node;
+        }
+        Node temproot = root;
+        Node parent;
+        while (true){
+            parent=temproot;
+            if (id<=temproot.ID) {
+                temproot = temproot.left;
+                if (temproot==null){
+                    parent.left=node;
+                }
+
+            }else {
+                temproot = temproot.right;
+                if (temproot==null){
+                    parent.right = node;
+                }
+            }
+
+
+        }
+
+    }
 }
